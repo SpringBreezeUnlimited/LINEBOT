7055237b47d3966fb03ac648b101412c56182105
remiriasukaretto <124414545+remiriasukaretto@users.noreply.github.com>
2026-05-08 13:46:17 +0000

    配色更新

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ DB_CONNECT_TIMEOUT = int(os.getenv("DB_CONNECT_TIMEOUT", "5"))
 
 OWNER_LINE_ID = os.getenv('OWNER_LINE_ID', '').strip()
 
-APP_VERSION = "v1.0.95"
+APP_VERSION = "v1.0.96"
 APP_RELEASED_AT = "2026-05-08 00:00 JST"
 
 FORCE_HTTPS = parse_bool_env("FORCE_HTTPS", True)
